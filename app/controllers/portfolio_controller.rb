adafdbfb55a02b190bab9def6ac6f44a5067bfac
Patrick Bradley <patrickbradley777@gmail.com>
2019-05-28 15:19:36 -0400

    return static portfolio json at path /portfolios

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,4 +1,11 @@
 class PortfolioController < ApplicationController
   def index
+    begin
+      portfolios = JSON.parse(File.read(File.join(Rails.root, "portfolio.json")))
+    rescue => e
+      render json: { "error": { "code": 500, "message": e } }
+    end
+
+    render json: portfolios
   end
 end

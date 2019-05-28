d34e6106da4cd82c5b3410dd0b34853e5a824604
Patrick Bradley <patrickbradley777@gmail.com>
2019-05-28 13:30:26 -0400

    added basic portfolio controller scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'portfolio/index'
   root 'home#landing'
   get 'home/landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

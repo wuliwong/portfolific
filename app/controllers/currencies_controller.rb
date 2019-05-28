8ed3e518213b8e20f5ecdfae630437732891b524
Patrick Bradley <patrickbradley777@gmail.com>
2019-05-28 15:13:06 -0400

    added static json response with currency codes at /currencies path

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,4 +1,11 @@
 class CurrenciesController < ApplicationController
   def index
+    begin
+      currencies = JSON.parse(File.read(File.join(Rails.root, "CurrencyCode.json")))
+    rescue => e
+      render json: { "error": { "code": 500, "message": e } }
+    end
+
+    render json: currencies
   end
 end

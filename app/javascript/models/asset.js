95405d285cee0b605532c6d6edb576fbde411470
Patrick Bradley <patrickbradley777@gmail.com>
2019-05-28 18:58:52 -0400

    fixed typo in currencyCode

--- app/javascript/models/asset.js
+++ app/javascript/models/asset.js
@@ -1,11 +1,11 @@
 import { observable } from 'mobx';
 
 export default class Asset {
-  @observable currecyCode;
+  @observable currencyCode;
   @observable amount;
 
   constructor(data) {
-    this.currecyCode = data.currecyCode;
+    this.currencyCode = data.currencyCode;
     this.amount = data.amount;
   }
 }

136436684916e559b284ed61a83964cadcf6c232
Patrick Bradley <patrickbradley777@gmail.com>
2019-05-28 15:01:10 -0400

    set paths for controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'currencies/index'
-  get 'countries/index'
-  get 'portfolio/index'
+  get '/currencies' => 'currencies#index'
+  get '/countries' => 'countries#index'
+  get '/portfolios' => 'portfolio#index'
   root 'home#landing'
-  get 'home/landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

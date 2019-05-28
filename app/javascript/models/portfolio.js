d36bb72fb115dad201f1a1afdbe6e9b811bc50b8
Patrick Bradley <patrickbradley777@gmail.com>
2019-05-28 19:01:37 -0400

    added methods to get the total value of a portfolio in a native currency

--- app/javascript/models/portfolio.js
+++ app/javascript/models/portfolio.js
@@ -1,12 +1,14 @@
 import { observable } from 'mobx';
 import County from './country';
 import Asset from './asset';
+import exchangeRateStore from '../stores/exchange_rate_store';
+import currencyStore from '../stores/currency_store';
+import countryStore from '../stores/country_store';
 
 export default class Portfolio {
   @observable name;
   @observable slug;
   @observable countryCode;
-  @observable country;
   @observable assets = [];
 
   constructor(data) {
@@ -19,5 +21,25 @@ export default class Portfolio {
         this.assets.push(new Asset(a));
       });
     }
+  }
+
+  totalValueForCountry(countryCode) {
+    let totalValue = 0;
+
+    _.each(this.assets, (asset) => {
+      totalValue += exchangeRateStore.exchangeRates[asset.currencyCode]*parseFloat(asset.amount)
+    });
+
+    return totalValue;
+  }
+
+  totalValueInNativeCurrency() {
+    return this.totalValueForCountry(this.countryCode)
+  }
+
+  nativeCurrencySymbol() {
+    const country = countryStore.findByCountryCode(this.countryCode);
+    console.log(country);
+    return currencyStore.currencySymbolForCode(country.currencyCode);
   }
 }
